2cb14354ac35fd021de0da51017fb6b5f09cd599
LTaylor171 <lt334@byu.edu>
2024-10-22 13:43:28 -0600

    GameDAO implemetation

--- server/src/main/java/dataaccess/memory/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryGameDAO.java
@@ -1,5 +1,37 @@
 package dataAccess.memory;
 
-public class MemoryGameDAO {
-    
+import dataAccess.GameDAO;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MemoryGameDAO implements GameDAO {
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+    // clear the database
+    public void clear(){
+        games.clear();
+    }
+
+    // create a new game
+    public void createGame(GameData gameData) {
+        games.put(gameData.gameID(), gameData);
+    }
+
+    // get the game data
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    // get all the games
+    public ArrayList<GameData> getAllGames() {
+        return new ArrayList<>(games.values());
+    }
+
+    // update the game
+    public void updateGame(GameData newGame) {
+        games.put(newGame.gameID(), newGame);
+    }
+
 }
