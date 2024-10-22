9fbb15a659c7a55eea4bd60f1a76f7071825ae14
LTaylor171 <lt334@byu.edu>
2024-10-22 13:29:23 -0600

    UserDAO implemetation

--- server/src/main/java/dataaccess/memory/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryUserDAO.java
@@ -1,5 +1,32 @@
 package dataAccess.memory;
 
-public class MemoryUserDAO {
+import dataAccess.UserDAO;
+import model.UserData;
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private final HashMap<String, UserData> users = new HashMap<>();
+    // clear the database
+    public void clear(){
+        users.clear();
+    }
+
+    // create a new user
+    @Override
+    public void createUser(UserData userData) {
+        users.put(userData.username(), userData);
+    }
+
+    // return user data
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    // check if user exists
+    @Override
+    public boolean existsUser(String username) {
+        return users.containsKey(username);
+    }
     
 }
